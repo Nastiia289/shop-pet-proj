493c20bd647c3e69a839ba5a4143e9de569e0e1b
unknown <nastja12129@gmail.com>
2023-01-26 00:25:05 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
-import React, { useState, useContext } from 'react'
-import { CreateProduct } from './components/createProduct';
-import { ErrorMessage } from './components/errorMessage';
-import { Loader } from './components/loader';
-import { Modal } from './components/modal';
-import { Product } from './components/product';
-import { ModalContext } from './context/modalContext';
-import { useProducts } from './hooks/products';
-import { IProduct } from './modules';
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import { AboutPage } from './pages/aboutPage';
+import { ProductsPage } from './pages/ProductsPage';
+import { Navigation } from './components/Navigation';
+
 
 function App() {
-  const { loading, products, error, addProduct } = useProducts()
-  const {modal, open: openModal, close: closeModal} = useContext(ModalContext)
-
-  const createHandler = (product: IProduct) => {
-    closeModal()
-    addProduct(product)
-  }
-
-  return (
-    <div className='container mx-auto max-w-2xl pt-5'>
-      {loading && <Loader />}
-      {error && <ErrorMessage error={error} />}
-      {products.map(product => <Product product={product} key={product.id} />)}
-
-      {modal && <Modal title='Create new product' onClose={() => closeModal()}>
-        <CreateProduct onCreate={createHandler}/>
-      </Modal>}
-
-      <button 
-        className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
-        onClick={()=>openModal()}
-      >+</button>
-    </div>
+  return(
+    <>
+    <Navigation/>
+     <Routes>
+      <Route path='/' element={<ProductsPage/>}/>
+      <Route path='/about' element={<AboutPage/>}/>
+    </Routes>
+    </>
   )
 }
 
